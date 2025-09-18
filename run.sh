922e9fc25a40f88ba156722c2be6396b3f630421
lsiwh37249 <lsiwh37249@gmail.com>
2025-09-18 13:15:00 +0900

    add : data 디렉토리 생성

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ sudo apt install python3-pip -y
 python3 -m pip install --upgrade pip
 python3 -m pip install streamlit
 cd cw_app
-nohup  streamlit run front/streamlit_app.py --server.address 0.0.0.0 --server.port 8501 
+nohup streamlit run front/'작업자별 작업 목록 조회.py' --server.address 0.0.0.0 --server.port 8501
+
